fa594c9abb8796a12b856914b50f2c1b13aa092d
JMarte03 <collegeis2much@gmail.com>
2025-01-31 15:44:18 -0400

    Hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,23 @@ module.exports = {
     './public/**/*.js',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bghWhite: '#f8f8f8',
+        gray: '#676767',
+        lightGray: '#d9d9d9', 
+      },
+      fontFamily: {
+        hanken: ['Hanken Grotesk', 'sans-serif'],
+        kanit: ['Kanit', 'sans-serif'],
+        prompt: ['Prompt', 'sans-serif'],
+        archivo: ['Archivo Black', 'sans-serif'],
+        gruppo: ['Gruppo', 'serif']
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    require('daisyui'),
+  ],
 }
 
