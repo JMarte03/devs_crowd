b33f35799e42ef5a26d3e3cf220593f3ee0c1728
JMarte03 <collegeis2much@gmail.com>
2025-01-31 16:58:43 -0400

    Features section

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,15 +7,13 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        bghWhite: '#f8f8f8',
+        bgWhite: '#f8f8f8',
         gray: '#676767',
         lightGray: '#d9d9d9', 
       },
       fontFamily: {
         hanken: ['Hanken Grotesk', 'sans-serif'],
         kanit: ['Kanit', 'sans-serif'],
-        prompt: ['Prompt', 'sans-serif'],
-        archivo: ['Archivo Black', 'sans-serif'],
         gruppo: ['Gruppo', 'serif']
       }
     },
